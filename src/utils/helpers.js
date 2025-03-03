cea9925a3f33aee45e9e27c79f76524b74928f0b
fahlevi20 <rozakalfi@gmal.com>
2025-03-03 16:04:20 +0700

    add new

--- src/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-// utils/helpers.js - Utility functions
-/**
- * Creates a delay using promises
- * @param {number} ms - Milliseconds to delay
- * @returns {Promise} - Promise that resolves after delay
- */
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
-module.exports = { delay };
